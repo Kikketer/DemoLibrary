c8d984052cab72eba74d3c4bbd85d21c3e99eece
Chris Weed <christopher.weed@workgrid.com>
2022-09-14 14:19:41 -0500

    🔥

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { captureMessage } from '@sentry/minimal'
 const createMessage = () => {
   console.log('Capturing a message')
   captureMessage('This is a test message from the library')
+  return 'The message was captured'
 }
 
 export { captureMessage }
